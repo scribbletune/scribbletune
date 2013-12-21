a894594fde12ff8e2f13c19ff67b9a62cd822743
Walmik <walmik.deshpande@gmail.com>
2013-12-20 16:20:02 -0800

    added patterns in a string format

--- app.js
+++ app.js
@@ -2,26 +2,56 @@ var Midi = require('./node_modules/jsmidgen/lib/jsmidgen');
 var fs = require('fs');
 var _ = require('lodash');
 
-//init file to write to
-var file = new Midi.File();
-//init Midi track
-var track = new Midi.Track();
-//add track to file
-file.addTrack(track);
-
 var Modes = require('./lib/modes');
-var mode = Modes.get('c', 3, 'lydian');
-
-//add a filter (counterpoint)
 var Filters = require('./lib/filters');
-mode = Filters.counterpoint(mode);
-//add another filter (randomize)
+var Patterns = require('./lib/patterns');
+
+
+/*
+	Example:
+	--------
+	var mode = Modes.get('f', 3, 'phrygian');
+	//modify the notes array 
+	mode = Filters.counterpoint(mode);
+	add another filter (randomize)
+	mode = Filters.randomize(mode);
+
+	OR simply define the array of notes you want
+	var mode = ['c2', '','','', 'c2', '','','','c2', '','','', 'c2 ', '','',''];
+	
+
+	//write the notes to a MIDI file
+	writeTrackToFile(mode, 'mode.mid');
+
+*/
+
+//generate a mode
+var mode = Modes.get('f', 2, 'phrygian');
 mode = Filters.randomize(mode);
+mode = mode.concat(mode);
+mode = Filters.patternize(mode, Patterns.triplets1);
+
+
 
 //write track
-_.map(mode, function(note){
-	track.addNote(0, note, 64);	//params = track, note with octave, ticks/interval/noteLength
-});
+writeTrackToFile(mode, 'mode.mid');
 
-//write track to Midi file
-fs.writeFileSync('mode.mid', file.toBytes(), 'binary');
+
+function writeTrackToFile(notesArr, fileName, noteLength) {
+	var noteLength = noteLength || 32;
+
+	//init file to write to
+	var file = new Midi.File();
+	//init Midi track
+	var track = new Midi.Track();
+	//add track to file
+	file.addTrack(track);
+
+	_.map(notesArr, function(note){
+		if(note !== '') track.addNote(0, note, noteLength);	//params = track, note with octave, ticks/interval/noteLength
+		else track.noteOff(1, note, noteLength);
+	});
+
+	//write track to Midi file
+	fs.writeFileSync(fileName, file.toBytes(), 'binary');
+}
