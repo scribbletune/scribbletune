8259f12d3863d4429b16a0866b6598e52552db97
Walmik <walmik.deshpande@gmail.com>
2013-12-27 21:29:43 -0800

    isolated writeMidiFile to a utils module

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
-var Midi = require('./node_modules/jsmidgen/lib/jsmidgen');
-var fs = require('fs');
-var _ = require('lodash');
+
 
 var Modes = require('./lib/modes');
 var Generate = require('./lib/generate');
 var Filters = require('./lib/filters');
 var Patterns = require('./lib/patterns');
-
+var Utils = require('./lib/utils');
 
 /*
-	Example:
+	Process:
 	--------
-	var mode = Modes.get('f', 3, 'phrygian');
-	//modify the notes array 
-	mode = Filters.counterpoint(mode);
-	add another filter (randomize)
-	mode = Filters.randomize(mode);
-
-	OR simply define the array of notes you want
-	var mode = ['c2', '','','', 'c2', '','','','c2', '','','', 'c2 ', '','',''];
-	
-
-	//write the notes to a MIDI file
-	writeTrackToFile(mode, 'mode.mid');
-
+	- Use the Modes module to get a bunch of notes or Manually set an array of notes which act as the palette
+	- Generate the required number of bars/beats from this palette
+	- Apply a pattern 
+	- Apply filters
+	- Render track to a Midi file
 */
 
 //var notes = Modes.get('f', 2, 'phrygian');
@@ -36,24 +26,6 @@ bar = Generate.bars(notes);
 console.log(bar);
 
 //write track
-//writeTrackToFile(bar, 'bar.mid');
+//Utils.writeTrackToFile(bar, 'bar.mid');
 
 
-function writeTrackToFile(notesArr, fileName, noteLength) {
-	var noteLength = noteLength || 32;
-
-	//init file to write to
-	var file = new Midi.File();
-	//init Midi track
-	var track = new Midi.Track();
-	//add track to file
-	file.addTrack(track);
-
-	_.map(notesArr, function(note){
-		if(note !== '') track.addNote(0, note, noteLength);	//params = track, note with octave, ticks/interval/noteLength
-		else track.noteOff(1, note, noteLength);
-	});
-
-	//write track to Midi file
-	fs.writeFileSync(fileName, file.toBytes(), 'binary');
-}
