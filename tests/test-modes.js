9fb173f5af38d96e31320a93eeabfe543d1fdad2
Walmik <walmik.deshpande@gmail.com>
2014-01-14 23:01:24 -0800

    added tests for octave isnumeric and is in the range of 1 to 8

--- tests/test-modes.js
+++ tests/test-modes.js
@@ -1,15 +1,45 @@
 var Modes = require('../lib/modes');
 
-//test if Modes.get returns the C Ionian mode (major scale) if no parameters are passed
-exports.test = function(test) {
+/*
+ * Test function with 0 arguments
+ * Calling Modes.get() should return C Ionian mode (C major scale) by default
+ */
+exports.testZeroArgs = function(test) {
 	test.equal(Modes.get().join(','), [ 'c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3', 'c4' ].join(','));
-	test.ok(true, "this assertion should pass");
+	test.ok(true);
   test.done();
 }
 
-//test if Modes.get throws an error if a wrong mode is passed
+
+
+/*
+ * Test whether octave is a numeric value
+ * Function call with a non numneric value should throw an Error
+ */
+exports.testIsOctaveNumeric = function(test) {
+	test.throws(function() { Modes.get('c', 's') }, Error);
+	test.ok(true);
+  test.done();
+}
+
+
+/*
+ * Test whether octave is between 1 and 8 (both inclusive)
+ * Function call with a non numneric value should throw an Error
+ */
+exports.testOctaveRange = function(test) {
+	test.throws(function() { Modes.get('c', 10) }, Error);
+	test.ok(true);
+  test.done();
+}
+
+
+/*
+ * Test whether a valid mode was requested
+ * Valid modes are listed in modes.js
+ */
 exports.testValidMode = function(test) {
-	test.throws(function() { Modes.get('c', 3, 'non_existent_mode'); }, Error, 'Wrong mode!');
-	test.ok(true, "this assertion should pass");
+	test.throws(function() { Modes.get('c', 3, 'non_existent_mode') }, Error, 'Wrong mode!');
+	test.ok(true);
   test.done();
 }
