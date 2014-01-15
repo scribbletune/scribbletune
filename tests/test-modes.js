fa7519d58ce2a7913b948d97b8b1a2409f0e2dc3
Walmik <walmik.deshpande@gmail.com>
2014-01-14 22:42:54 -0800

    added test to validate mode

--- tests/test-modes.js
+++ tests/test-modes.js
@@ -1,7 +1,15 @@
 var Modes = require('../lib/modes');
-console.log(Modes.get());
+
+//test if Modes.get returns the C Ionian mode (major scale) if no parameters are passed
 exports.test = function(test) {
 	test.equal(Modes.get().join(','), [ 'c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3', 'c4' ].join(','));
+	test.ok(true, "this assertion should pass");
+  test.done();
+}
+
+//test if Modes.get throws an error if a wrong mode is passed
+exports.testValidMode = function(test) {
+	test.throws(function() { Modes.get('c', 3, 'non_existent_mode'); }, Error, 'Wrong mode!');
 	test.ok(true, "this assertion should pass");
   test.done();
 }
