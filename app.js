f171315ff3eac724cefe20c2686d72a88c489cd4
Walmik <walmik.deshpande@gmail.com>
2014-03-25 17:21:49 -0700

    Added multiple bar creation

--- app.js
+++ app.js
@@ -12,15 +12,48 @@ var Utils = require('./lib/utils');
 
 
 //var notes = ['d#4', 'd4', 'd#4', 'c4', 'f4', 'f#4'];
-var notes = Modes.get('d#', 3, 'phrygian');
+var notes = Modes.get('d#', 3 , 'phrygian');
 
-bar = Generate.bars({
+/*var bar = Generate.bars({
 	notesArr: notes,
 	pattern: Patterns.generate(),
 	bars: 8,
 	randomize: true
+});*/
+
+notes = Modes.get('g#', 4  , 'lydian');
+var bar1 = Generate.bars({
+	notesArr: notes,
+	pattern: Patterns.fancy[0],
+	bars: 2,
+	randomize: true
 });
 
+notes = Modes.get('f# ', 4 , 'lydian');
+var bar2 = Generate.bars({
+	notesArr: notes,
+	pattern: Patterns.fancy[0],
+	bars: 2,
+	randomize: true
+});
+
+notes = Modes.get('g#', 4 , 'lydian');
+var bar3 = Generate.bars({
+	notesArr: notes,
+	pattern: Patterns.fancy[0],
+	bars: 2,
+	randomize: true
+});
+
+notes = Modes.get('f# ', 4  , 'lydian');
+var bar4 = Generate.bars({
+	notesArr: notes,
+	pattern: Patterns.fancy[0],
+	bars: 2,
+	randomize: true
+});
+
+var bar = bar1.concat(bar2, bar3, bar4);
 //Apply a filter
 bar = Filters.mergeDuplicates(bar);
 Utils.visualize(bar);
