8da3e30ef473d05d0d5cdaa951441f07e76a77a2
Walmik <walmik.deshpande@gmail.com>
2014-04-13 18:39:19 -0700

    started adding chord creation

--- app.js
+++ app.js
@@ -11,14 +11,47 @@ var Utils = require('./lib/utils');
 */
 
 //var notes = ['d#4', 'd4', 'd#4', 'c4', 'f4', 'f#4'];
-var notes = Modes.get('d#', 2 , 'phrygian');
+var notes1 = Modes.get('f#', 3 , 'ionian');
+var notes2 = Modes.get('f#', 3 , 'lydian');
 
-var bar = Generate.bars({
-	notesArr: notes,
-	pattern: Patterns.generic[7],
-	bars: 1,
+var bar1 = Generate.bars({
+	notesArr: notes1,
+	pattern: Patterns.fancy[0],
+	bars: 2,
 	randomize: true
 });
+
+var bar2 = Generate.bars({
+	notesArr: notes2,
+	pattern: Patterns.fancy[1],
+	bars: 2,
+	randomize: true
+});
+
+var bar3 = Generate.bars({
+	notesArr: notes1,
+	pattern: Patterns.fancy[2],
+	bars: 2,
+	randomize: true
+});
+
+var bar4 = Generate.bars({
+	notesArr: notes2,
+	pattern: Patterns.fancy[3],
+	bars: 2 ,
+	randomize: true
+});
+
+var notes3 = Modes.get('f#', 3 , 'mixolydian');
+var bar5 = Generate.bars({
+	notesArr: notes3,
+	pattern: Patterns.fancy[2],
+	bars: 2,
+	randomize: true
+});
+
+bar = bar1.concat(bar2,bar3,bar4,bar5);
+
 
 
 //Apply a filter
