329c0ee58e378bad155afe6e1827434c700ed384
Walmik <walmik.deshpande@gmail.com>
2014-07-13 05:46:16 -0700

    Formatted testMode and started testGenerate

---
+++ test/testGenerate.js
@@ -0,0 +1,42 @@
+/**
+ * Test Scribbletune's Generate object
+ * -----------------------------------
+ */
+'use strict';
+
+var st = require('../lib/scribbletune');
+
+describe('Scribbletune:Generate', function(){
+
+	/**
+	 * Scribbletune.generate.clip()
+	 */
+	describe('clip()', function(){
+
+		it(
+			'should use a default pattern if one is not provided', 
+			function(){
+				(function(){
+
+				}).should.not.throw();
+			}
+		);
+
+		
+		it(
+			'should use a default set of notes if notes are not provided', 
+			function(){
+			
+			}
+		);
+
+
+		it(
+			'should check if a valid set of notes is provided', 
+			function(){
+
+			}
+		);
+
+	});
+});
--- test/testMode.js
+++ test/testMode.js
@@ -1,36 +1,66 @@
-var obj = {};
-var mode = require('../lib/ext/mode')(obj);	//the mode module expects an object that it can latch on itself to
+/**
+ * Test Scribbletune's Mode object
+ * -------------------------------
+ */
+
+'use strict';
+
+var st = require('../lib/scribbletune');
 
 describe('Scribbletune:Mode', function(){
+
+	/**
+	 * Scribbletune.mode.get()
+	 */
 	describe('get()', function(){
 		
-		it('should check if provided root note is valid: fail h', function(){
-			(function(){
-		    	obj.mode.get('h');
-			}).should.throw();
+		it(
+			'should check if provided root note is valid: fail h', 
+			function(){
+				(function(){
+			    	st.mode.get('h');
+				})
+				.should.throw();
+			}
+		);
+
+
+		it(
+			'should check if provided root note is valid: pass c#', 
+			function(){
+				(function(){
+			    	st.mode.get('c#');
+				}
+			).should.not.throw();
 		});
 
-		it('should check if provided root note is valid: pass c#', function(){
-			(function(){
-		    	obj.mode.get('c#');
-			}).should.not.throw();
-		});
 
-		it('should check if provided mode is valid', function(){
-			(function(){
-				obj.mode.get('c', 'rubbish');
-			}).should.throw();
-		});
+		it(
+			'should check if provided mode is valid', 
+			function(){
+				(function(){
+					st.mode.get('c', 'rubbish');
+				}).should.throw();
+			}
+		);
 
-		it('should return [c, d, e, f, g, a, b] when root is C and mode is ionian', function(){
-			var cIonian = obj.mode.get('c', 'ionian');
-			cIonian.join('').should.equal('cdefgab');
-		});
 
-		it('should return [d, e, f, g, a, b, c] when root is D and mode is dorian', function(){
-			var dDorain = obj.mode.get('d', 'dorian');
-			dDorain.join('').should.equal('defgabc');
-		});
+		it(
+			'should return [c, d, e, f, g, a, b] when root is C and mode is ionian', 
+			function(){
+				var cIonian = st.mode.get('c', 'ionian');
+				cIonian.join('').should.equal('cdefgab');
+			}
+		);
+
+
+		it(
+			'should return [d, e, f, g, a, b, c] when root is D and mode is dorian', 
+			function(){
+				var dDorain = st.mode.get('d', 'dorian');
+				dDorain.join('').should.equal('defgabc');
+			}
+		);
 
 	});
 });
