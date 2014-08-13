fb80bd4585c953cf12e7df19edd8913973cfb606
Walmik <walmik.deshpande@gmail.com>
2014-08-12 17:22:42 -0700

    Tweaked examples

--- examples/bassline.js
+++ examples/bassline.js
@@ -6,8 +6,11 @@ var scribbletune = require('../lib/scribbletune');
 
 var clip = 
 	scribbletune.generate.clip({
-		notes: ['c2'], 
-		pattern: '--x---x---x---x-'
+		notes: scribbletune.mode.get('c', 'phrygian', 2).slice(0, 3), 
+		pattern: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+		sizzle: true,
+		sizzleMap: 'x-------x-----x-x-------------x-x-------x-----x-x-------------x-',
+		shuffle: true
 	});
 
 scribbletune.midi.writeToFile(clip);
--- examples/harmony.js
+++ examples/harmony.js
@@ -2,13 +2,13 @@ var _ = require('lodash');
 var scribbletune = require('../lib/scribbletune');
 
 //start with a bunch of notes
-var notes = 'c#,f#,g#'.split(',');
+var notes = 'd#,g,f#'.split(',');
 
 //set a mode to be used
-var modesArr= ['ionian', 'dorian'];
+var modesArr= ['dorian', 'aeolian'];
 
 //set an octave
-var octave = 2;
+var octave = 3;
 
 //generate an array of arrays of ionian modes of the notes
 var modesArr = notes.map(function(el, idx){
@@ -24,13 +24,14 @@ var notesArr = _.union(notesWithOctaves, intersectedModes);
 
 var clip = 
 	scribbletune.generate.clip({
-		notes: ['c2'], 
+		notes: notesArr, 
 		//pattern: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-		pattern: 'x-------x-----x-x-------------x-',
-		//sizzle: true,
-		//sizzleMap: 'x--------x--------x----x----x---'
+		//pattern: 'x-------x-----x-x-------------x-',
+		pattern: 'xxxxx-x--xx-xx-xxxx---x-xx-xxxxx'.replace(/-/g, '_'),
+		sizzle: true,
+		sizzleMap: 'x--------x--------x----x----x---',
 		//sizzleMap: 'x-------x-----x-x-------------x-'
-		//shuffle: true
+		shuffle: true
 	});
 
 
