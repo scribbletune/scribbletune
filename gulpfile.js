22e914190885015542e376c8338cac3db1ee2cbf
Walmik Deshpande <walmik.deshpande@gmail.com>
2015-06-20 21:03:06 -0700

    Added jscs. Fixed issues from jscs.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
-	jshint = require('gulp-jshint');
+	jshint = require('gulp-jshint'),
+	jscs = require('gulp-jscs');
 
 gulp.task('scripts', function() {
 	return gulp.src(['./lib/scribbletune.js', './lib/ext/*.js'])
 		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
+		.pipe(jshint.reporter('default'))
+		.pipe(jscs());
 });
