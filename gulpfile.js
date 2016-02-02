9295c7cb356f572d641c4653eb3983567639b9cd
Walmik Deshpande <walmik.deshpande@gmail.com>
2016-02-02 09:58:03 -0800

    Added coverage via gulp istanbul

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
 gulp.task('clean', function() {
 	return gulp.src(['./lib/scribbletune.js', './lib/ext/*.js', './test/*.js'])
@@ -9,4 +11,17 @@ gulp.task('clean', function() {
 		.pipe(jscs());
 });
 
-gulp.task('default', ['clean']);
+gulp.task('pre-coverage', function() {
+	return gulp.src('lib/*.js')
+		.pipe(istanbul())
+		// This overwrites `require` so it returns covered files
+		.pipe(istanbul.hookRequire());
+});
+
+gulp.task('coverage', ['pre-coverage'], function() {
+	gulp.src('index.js')
+		.pipe(mocha({reporter: 'spec'}))
+		.pipe(istanbul.writeReports());
+});
+
+gulp.task('default', ['clean', 'coverage']);
