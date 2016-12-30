d8a50147d4c772403a25e57bee82e0d71dfb4f22
Walmik Deshpande <walmik.deshpande@gmail.com>
2016-12-29 23:57:37 -0800

    Fix maj7, min7 chords

--- test/chord.js
+++ test/chord.js
@@ -24,6 +24,18 @@ test('Scribbletune::chord', t => {
 	);
 
 	t.equal(
+		chord.getChord('Cmaj7').join(),
+		'c4,e4,g4,b4',
+		'C Major is C E G B'
+	);
+
+	t.equal(
+		chord.getChord('Cmin7').join(),
+		'c4,d#4,g4,a#4',
+		'C Minor is C D# G A#'
+	);
+
+	t.equal(
 		chord.getChord('Csus2').join(),
 		'c4,d4,g4',
 		'C Sus2 is C D G'
