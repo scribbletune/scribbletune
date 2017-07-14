03c1ec97a3aa4b5854ed72aa996f53321997cefa
Garrett Credi <gcc@ameritech.net>
2017-07-14 15:30:54 +0000

    Revert "Add middle C octave changer"
    This reverts commit fa9c757123d1329984b2bba0d453bcef066d2e7d.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-'use strict';
-const scale = require('./mode');
-const chord = require('./chord');
-const clip = require('./clip');
-const pattern = require('./pattern');
-const midi = require('./midi');
-const scales = require('./modes');
-function setMiddle(octaveIndex){
-    midi.setMiddle(octaveIndex);
-    //Trans is the positive octave transposition
-    //If trans is 1, all notes will be shifted up one octave
-}
-let modes = Object.keys(scales);
-
-// Allow scale to be denoted by mode as well
-module.exports = {mode: scale, scale, chord: chord.getChord, listChords: chord.listChords, modes, scales: modes, clip, pattern, midi: midi.midi, setMiddle};
+'use strict';const scale = require('./mode');const chord = require('./chord');const clip = require('./clip');const pattern = require('./pattern');const midi = require('./midi');const scales = require('./modes');let modes = Object.keys(scales);// Allow scale to be denoted by mode as wellmodule.exports = {mode: scale, scale, chord: chord.getChord, listChords: chord.listChords, modes, scales: modes, clip, pattern, midi};
--- src/midi.js
+++ src/midi.js
@@ -3,14 +3,7 @@
 const fs = require('fs');
 const assert = require('assert');
 const jsmidgen = require('jsmidgen');
-var transposition = 0;
-/**
- * Takes an integer and transposes all notes to a different middle C octave.
- * @param {Integer} octaveIndex		The new octave for middle C.
- */
-function setMiddle(octaveIndex){
-	transposition = octaveIndex;
-}
+
 /**
  * Take an array of note objects to generate a MIDI file in the same location as this method is called
  * @param  {Array} notes    Notes are in the format: {note: 'c3', level: 127, length: 64}
@@ -29,14 +22,6 @@ const midi = (notes, fileName) => {
 		// only the first noteOn (or noteOff) needs the complete arity of the function call
 		// subsequent calls need only the first 2 args (channel and note)
 		if (noteObj.note) {
-			var note = noteObj.note[0];
-			//Get the octave identifier (2 for a2, 5 for e5)
-			var oct = parseInt(note.slice(1,note.length));
-			//Parse the octave into an integer
-			oct+=transposition;
-			//Transpose the octave
-			noteObj.note = note[0]+oct.toString();
-			console.log(note);
 			if (typeof noteObj.note === 'string') {
 				track.noteOn(0, noteObj.note, noteObj.length, level); // channel, pitch(note), length, velocity
 				track.noteOff(0, noteObj.note, noteObj.length, level);
@@ -44,11 +29,12 @@ const midi = (notes, fileName) => {
 				track.addChord(0, noteObj.note, noteObj.length, level);
 			}
 		} else {
-			track.noteOff(0, '', noteObj.length);
+			track
+				.noteOff(0, '', noteObj.length);
 		}
 	});
 
 	fs.writeFileSync(fileName, file.toBytes(), 'binary');
 }
 
-module.exports = {midi, setMiddle};
+module.exports = midi;
