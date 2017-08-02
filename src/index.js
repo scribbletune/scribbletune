9cc718fbc154cd57342c85cc841258682306977c
ddxtanx <gcc@ameritech.net>
2017-08-02 14:33:43 -0500

    Improve note transposition

--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ const setMiddleC = require('./setMiddleC');
 let modes = Object.keys(scales);
 
 // Allow scale to be denoted by mode as well
-module.exports = {mode: scale, scale, chord: chord.getChord, listChords: chord.listChords, modes, scales: modes, clip, pattern, midi, setMiddleC: setMiddleC.setMiddleC};
+module.exports = {mode: scale, scale, chord: chord.getChord, listChords: chord.listChords, modes, scales: modes, clip, pattern, midi, setMiddleC: setMiddleC.setMiddleC, transposeNote: setMiddleC.transposeNote};
