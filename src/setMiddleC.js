aa7b108d5dd61c9084fed2656f8b3a92e9d6691b
ddxtanx <gcc@ameritech.net>
2017-08-03 13:15:24 -0500

    Add space after if & arrow notation

--- src/setMiddleC.js
+++ src/setMiddleC.js
@@ -30,7 +30,7 @@ function transposeOctave(initialOctave) {
  * @param {Integer} octave Optional octave to transpose to  
  * @return {String(s)} 	The correctly transposed note(s)
  */
-function transposeNote(noteArg, octave) {
+const transposeNote = (noteArg, octave) => {
     let note;
 	assert(noteArg !== undefined && (typeof noteArg == 'string' || typeof noteArg == 'object') && noteArg.note === undefined && noteArg[0] !== undefined, 'NoteArg must contain a note that is either an array or a string.');
 	assert(Number.isInteger(octave) || octave===undefined, 'Octave must be an integer');
@@ -61,16 +61,16 @@ function transposeSingle(note, noteIndex, octave) {
 	assert(typeof note === 'string', 'Note must be a string.');
 	assert(Number.isInteger(octave) || octave === undefined, 'If octave is passed, it must be an integer.');
     let index = 1;
-	if(isNaN(note[1])) {
+	if (isNaN(note[1])) {
 		//Test if note is a single character like 'a5' or if it is like 'ab5'
 		index = 2;
 	}
 	let oct = parseInt(note.slice(index,note.length));
-	if(noteIndex==0) {
+	if (noteIndex==0) {
 		startOctave = oct;
 	}
 	//Parse the octave into an integer
-	if(octave) {
+	if (octave) {
 		oct = octave + (oct-startOctave);
 	} else {
 		oct += transposition;
