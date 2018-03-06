1c25be626a2a4e17dacae5542f4285ae8fc7bda7
Walmik Deshpande <walmik.deshpande@gmail.com>
2018-03-06 11:57:19 -0800

    Move to tonal
    * Move to tonal for chords and scales.
    * Remove functionality for transposing. Internally we ll use tonal to
    transpose and externally, folks can just use the adjusted octave.
    * Remove modes (tonal will care of listing scales)
    * Add alpha release to a full version

--- src/midi.js
+++ src/midi.js
@@ -3,7 +3,6 @@
 const fs = require('fs');
 const assert = require('assert');
 const jsmidgen = require('jsmidgen');
-const transpose = require('./transpose');
 
 /**
  * Take an array of note objects to generate a MIDI file in the same location as this method is called
@@ -23,8 +22,6 @@ const midi = (notes, fileName) => {
 		// only the first noteOn (or noteOff) needs the complete arity of the function call
 		// subsequent calls need only the first 2 args (channel and note)
 		if (noteObj.note) {
-			// Transpose the note to the correct middle C (in case middle C was changed)
-			noteObj.note = transpose.transposeNote(noteObj.note);
 			if (typeof noteObj.note === 'string') {
 				track.noteOn(0, noteObj.note, noteObj.length, level); // channel, pitch(note), length, velocity
 				track.noteOff(0, noteObj.note, noteObj.length, level);
