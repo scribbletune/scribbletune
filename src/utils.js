e491896de40673c70dd8ef6c76de4e00b0a74087
Walmik <wdeshpande@paypal.com>
2018-03-24 14:16:53 -0700

    Use Tonal for scales directly

--- src/utils.js
+++ src/utils.js
@@ -22,7 +22,6 @@ const utils = {
 	/**
 	 * Return an array of numbers relative to maxLevel || 127 ordered in a Sine wave format
 	 * This is used by the `sizzle` param of the `clip` method to add a rudimentary variation to the accent of each note
-	 * e.g. 
 	 * @param  {Number} maxLevel A number between not more than 127
 	 * @return {Array}  Example output [63, 90, 110, 127, 110, 90, 63, 0, 63, 90, 110, 127, 110, 90, 63, 0]
 	 */
