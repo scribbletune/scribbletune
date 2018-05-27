8a92a2b3e2cb5b1b93e8a1b3381bb9cf2c6331a2
Walmik <walmik.deshpande@gmail.com>
2018-05-27 13:25:16 -0700

    Provision chords

--- examples/chords.js
+++ examples/chords.js
@@ -2,7 +2,7 @@
 
 const scribble = require('../src/');
 let clip = scribble.clip({
-	notes: ['F#m', 'C#m', 'DM', 'Bm', 'EM', 'AM', 'DM', 'C#m', 'AM'],
+	notes: 'F#m C#m DM Bm EM AM DM C#m AM',
 	pattern: 'x_x_x_--'.repeat(8),
 	sizzle: true
 });
