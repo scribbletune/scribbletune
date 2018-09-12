d9c70a41b1bcb2e85d1194017982b0843ba29f4c
Walmik Deshpande <walmik.deshpande@gmail.com>
2018-09-12 15:38:47 -0700

    Add pan & volume support for browser clip

--- src/browserClip.js
+++ src/browserClip.js
@@ -7,7 +7,7 @@ const defaultSubdiv = '4n';
  * Take a Tone.js Player and return a function that can be used
  * as the callback in Tone.Sequence https://tonejs.github.io/docs/r12/Sequence
  */
-const _getPlayerSeqFn = (player) => {
+const _getPlayerSeqFn = player => {
 	return (time, el) => {
 		if (el === 'x') {
 			player.start(time);
@@ -21,7 +21,7 @@ const _getPlayerSeqFn = (player) => {
  * Take an object literal which has a Tone.js instrument and return a function that can be used
  * as the callback in Tone.Sequence https://tonejs.github.io/docs/r12/Sequence
  */
-const _getInstrSeqFn = (params) => {
+const _getInstrSeqFn = params => {
 	var counter = 0;
 	return (time, el) => {
 		if (el === 'x' && params.notes[counter]) {
@@ -40,7 +40,7 @@ const _getInstrSeqFn = (params) => {
  * Take an object literal which has a Tone.js sampler and return a function that can be used
  * as the callback in Tone.Sequence https://tonejs.github.io/docs/r12/Sequence
  */
-const _getSamplerSeqFn = (params) => {
+const _getSamplerSeqFn = params => {
 	var counter = 0;
 	return (time, el) => {
 		if (el === 'x' && params.notes[counter]) {
@@ -73,38 +73,45 @@ module.exports = params => {
 	}
 
 	/*
-	1. The params object can be used to pass a sample (sound source) OR a synth(Synth/FMSynth/AMSynth etc).
-	Scribbletune will then create a Tone.js Player or Tone.js Instrument respectively
+	1. The params object can be used to pass a sample (sound source) OR a synth(Synth/FMSynth/AMSynth etc) or samples.
+	Scribbletune will then create a Tone.js Player or Tone.js Instrument or Tone.js Sampler respectively
 	2. It can also be used to pass a Tone.js Player object or instrument that was created elsewhere
 	(mostly by Scribbletune itself in the channel creation method)
 	Either ways, a pattern is required and it will be used to create a playable Tone.js Sequence
 	 */
+
+	const panVol = new Tone.PanVol(params.pan || 0, params.volume || -12);
+
 	if (params.sample) {
 		// This implies, the clip is probably being hand created by the user with a audio sample
-		params.player = new Tone.Player(params.sample).toMaster();
-	}
-
-	if (params.player) {
-		// This implies, a player object was already created (either by user or mostly by Scribbletune during channel creation)
-		return new Tone.Sequence(_getPlayerSeqFn(params.player), utils.expandStr(params.pattern), defaultSubdiv);
+		params.player = new Tone.Player(params.sample);
 	}
 
 	if (params.samples) {
-		params.sampler = new Tone.Sampler(params.samples).toMaster();
-	}
-
-	if (params.sampler) {
-		return new Tone.Sequence(_getSamplerSeqFn(params), utils.expandStr(params.pattern), defaultSubdiv);
+		params.sampler = new Tone.Sampler(params.samples);
 	}
 
 	if (params.synth) {
 		// This implies, the synth is probably being hand created by the user with an available Tone synth
-		params.instrument = new Tone[params.synth]().toMaster();
+		params.instrument = new Tone[params.synth]();
+	}
+
+	if (params.player) {
+		params.player = params.player.chain(panVol, Tone.Master);
+		// This implies, a player object was already created (either by user or by Scribbletune during channel creation)
+		return new Tone.Sequence(_getPlayerSeqFn(params.player), utils.expandStr(params.pattern), params.subdiv || defaultSubdiv);
+	}
+
+	if (params.sampler) {
+		params.sampler = params.sampler.chain(panVol, Tone.Master);
+		// This implies, a sampler object was already created (either by user or by Scribbletune during channel creation)
+		return new Tone.Sequence(_getSamplerSeqFn(params), utils.expandStr(params.pattern), params.subdiv || defaultSubdiv);
 	}
 
 	if (params.instrument) {
-		// This implies, the instrument was already created (either by user or mostly by Scribbletune during channel creation)
+		params.instrument = params.instrument.chain(panVol, Tone.Master);
+		// This implies, the instrument was already created (either by user or by Scribbletune during channel creation)
 		// Unlike player, the instrument needs the entire params object to construct a sequence
-		return new Tone.Sequence(_getInstrSeqFn(params), utils.expandStr(params.pattern), defaultSubdiv);
+		return new Tone.Sequence(_getInstrSeqFn(params), utils.expandStr(params.pattern), params.subdiv || defaultSubdiv);
 	}
 };
