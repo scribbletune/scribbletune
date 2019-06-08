22778d2da7af5bab4be05b6804fabfe2de65a5a0
Walmik <walmik.deshpande@gmail.com>
2019-06-07 17:12:30 -0700

    Fix coverage targets and bump number

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,12 @@ module.exports = {
   },
   testRegex: './tests/.*.spec.ts',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  collectCoverageFrom: ['**/*.{ts}', '!**/node_modules/**'],
   coverageThreshold: {
     global: {
-      statements: 31.88,
-      branches: 22.25,
-      functions: 22.22,
-      lines: 32.18,
+      statements: 50,
+      branches: 25,
+      functions: 50,
+      lines: 50,
     },
   },
 };
