bd41b3cfed176ac84c78e323032987e24697092f
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2020-07-08 22:16:06 -0400

    transpose chords down in the example so root is in 2nd octave

--- examples/chords.js
+++ examples/chords.js
@@ -23,7 +23,7 @@ scribble.addChord(['1P', '5P', '8P', '22P', '24M', '26P'], [], 'fullM');
 scribble.addChord(['1P', '5P', '8P', '22P', '24m', '26P'], [], 'fullm');
 
 const clip3 = scribble.clip({
-  notes: ['Efullm', 'GfullM', 'GfullM', "AfullM"],
+  notes: ['Efullm2', 'GfullM2', 'GfullM2', "AfullM2"],
   pattern: 'x___xx_x________',
 });
 
